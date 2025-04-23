2ac274b7656d2719f7906e424d9d38a26085a2d5
mSharjeel589 <msharjeel589@gmail.com>
2025-04-23 17:36:36 +0500

    project setup + tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// import "./App.css";
+
+function App() {
+  return (
+    <h1 className="text-4xl font-bold text-center text-red-500">
+      Hello Abstract!
+    </h1>
+  );
+}
+
+export default App;
